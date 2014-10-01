132d773b4d873c9552fb14fa0109159a23f97e9f
unknown <o013580c@k106-9.student.staffs.ac.uk>
2014-10-01 11:29:31 +0100

    created a player class

---
+++ my_first_repo/player.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace my_first_repo
+{
+	public class player
+	{
+		
+		int x = 5;
+		int y = 7;
+		int score = 0;
+		string name;
+		
+		public player ()
+		{	
+			
+		}
+		
+		public void getName()
+		{
+			
+		}
+		
+		public int getX()
+		{
+			
+		}
+		
+		
+	}
+}
+
